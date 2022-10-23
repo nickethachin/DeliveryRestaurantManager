206250b30f748cfc183da465f8d0465bdc7a5450
nickethachin <nickethachin@gmail.com>
2022-10-24 00:26:26 +0700

    Change material table to DataGrid

--- frontend/src/components/MaterialManager/MaterialTable.jsx
+++ frontend/src/components/MaterialManager/MaterialTable.jsx
@@ -1,41 +1,73 @@
+import { DataGrid } from '@mui/x-data-grid';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-} from '@mui/material';
-import { useSelector } from 'react-redux';
-import MaterialItem from './MaterialRow';
+	deleteMaterial,
+	openEditor,
+} from '../../features/materials/materialSlice';
+import DeleteButton from '../DeleteButton';
+import EditButton from '../EditButton';
 
 const MaterialTable = () => {
 	const { materials } = useSelector(
 		(state) => state.materials
 	);
+	const dispatch = useDispatch();
+
+	const handleUpdate = (material) => {
+		dispatch(openEditor(material));
+	};
+
+	const handleDelete = (id) => {
+		dispatch(deleteMaterial(id));
+	};
+
+	const actionButtons = ({ row }) => {
+		return (
+			<>
+				<EditButton action={() => handleUpdate(row)} />
+				<DeleteButton
+					action={() => handleDelete(row._id)}
+				/>
+			</>
+		);
+	};
+	const columns = [
+		{ headerName: 'Name', field: 'name', flex: 1 },
+		{ headerName: 'Type', field: 'type' },
+		{ headerName: 'Unit', field: 'unit' },
+		{
+			headerName: 'updatedAt',
+			field: 'updatedAt',
+			width: 150,
+			type: 'dateTime',
+			renderCell: (params) =>
+				new Date(params.row.updatedAt).toLocaleString(
+					'en-GB',
+					{
+						timeStyle: 'short',
+						dateStyle: 'short',
+					}
+				),
+		},
+		{
+			headerName: 'Action',
+			field: 'action',
+			renderCell: actionButtons,
+		},
+	];
 	return (
-		<TableContainer>
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell>Name</TableCell>
-						<TableCell>Type</TableCell>
-						<TableCell>Unit</TableCell>
-						<TableCell>Action</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{materials !== null
-						? materials.map((material) => (
-								<MaterialItem
-									key={material._id}
-									material={material}
-								/>
-						  ))
-						: null}
-				</TableBody>
-			</Table>
-		</TableContainer>
+		<DataGrid
+			columns={columns}
+			rows={materials === [] ? [] : materials}
+			autoHeight
+			disableSelectionOnClick={true}
+			getRowId={(row) => row._id}
+			initialState={{
+				sorting: {
+					sortModel: [{ field: 'updatedAt', sort: 'desc' }],
+				},
+			}}
+		/>
 	);
 };
 
