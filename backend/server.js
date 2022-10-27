69c2978308a58266ab767ab13ca8a95bf83fddcb
nickethachin <nickethachin@gmail.com>
2022-10-27 23:55:09 +0700

    Deployable server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config({
@@ -11,20 +12,6 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 const app = express();
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('frontend/build'));
-	app.get('*', (req, res) =>
-		res.sendFile(
-			path.resolve(
-				__dirname,
-				'frontend',
-				'build',
-				'index.html'
-			)
-		)
-	);
-}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,6 +27,31 @@ app.use('/api/itemsets', require('./routes/itemsetRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/expenses', require('./routes/expenseRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	app.use(
+		express.static(
+			path.join(__dirname, '../frontend/build')
+		)
+	);
+
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(
+				__dirname,
+				'../',
+				'frontend',
+				'build',
+				'index.html'
+			)
+		)
+	);
+} else {
+	app.get('/', (req, res) =>
+		res.send('Please set to production')
+	);
+}
 
 app.use(errorHandler);
 
